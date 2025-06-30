1690e2c4fbc07ff0e943eca9e86992a769ecf74d
Mulat Yazew <gebremedhnyazewaait@gmail.com>
2025-06-30 12:36:46 +0300

    day 02 variables.py file updated

--- exercises/day_02_variables/variables.py
+++ exercises/day_02_variables/variables.py
@@ -11,21 +11,33 @@ is_married = False
 is_true = True
 is_light_on = False
 
+# Multiple variable declaration
 first_name, last_name, Full_name, country, city, age, year, is_married, is_true, is_light_on = "Jossy", "Teshome", "Jossy Teshome", "Ethiopia", "Addis Ababa", 30, 2023, False, True, False
 # Exercises: Level 2
 # Check the data type of all your variables using type() built-in function
 print("Variables:", type(first_name), type(last_name), type(Full_name), type(country), type(city), type(age), type(year), type(is_married), type(is_true), type(is_light_on))
-len_first = len(first_name) # Find the length of your first name
-len_last = len(last_name) # Find the length of your last name
-print(len_first == len_last) # Compare the length of your first name and your last name
-num_one, num_two = 5, 4 # Declare two variables num_one and num_two
-total = num_one + num_two # Add num_one and num_two and assign the result to a variable total
-diff = num_one-num_two # Subtract num_two from num_one and assign the result to a variable diff
-product = num_one * num_two # Multiply num_one and num_two and assign the result to a variable product
-division = num_one / num_two # Divide num_one by num_two and assign the result to a variable division
-remainder = num_two%num_one # Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
-exp = num_one**num_two # Calculate num_one to the power of num_two and assign the value to a variable exp
-floor_division = num_one // num_two # Use floor division to find num_one divided by num_two and assign the value to a variable floor_division
+# Find the length of your first name
+len_first = len(first_name)
+# Find the length of your last name
+len_last = len(last_name)
+# Compare the length of your first name and your last name
+print(len_first == len_last)
+# Declare two variables num_one and num_two 
+num_one, num_two = 5, 4 
+# Add num_one and num_two and assign the result to a variable total
+total = num_one + num_two
+# Subtract num_two from num_one and assign the result to a variable diff 
+diff = num_one-num_two 
+# Multiply num_one and num_two and assign the result to a variable product
+product = num_one * num_two 
+# Divide num_one by num_two and assign the result to a variable division
+division = num_one / num_two
+# Use modulus division to find num_two divided by num_one and assign the value to a variable remainder 
+remainder = num_two%num_one 
+# Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_one**num_two 
+# Use floor division to find num_one divided by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two 
 radius = 30
 pi = 3.14
 area_of_circle = pi*radius**2

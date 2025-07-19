6aca3c9e035057d12e4dcc597ca9d281bde55359
Yosef Teshome <joseteshe2017@gmail.com>
2025-07-19 15:48:49 +0300

    day 14 higher order functions

---
+++ exercises/day_14_higher_order_functions/higher_order_functions.py
@@ -0,0 +1,24 @@
+#Exercises: Day 14
+
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#Exercises: Level 1
+#1. Explain the difference between map, filter, and reduce.
+'''
+    - map - Applies a function to each element of an iterable 
+           (like a list) and returns a new iterable (like a list or map object).
+           - Syntax map(function, iterable)
+    - filter -Applies a condition (a function that returns True or False) and 
+              keeps only the elements that meet the condition.
+             - Syntax filter(condition, iterable)
+    - reduce (needs import from functools) - Takes all elements and reduces them to a 
+              single value by applying a function cumulatively.
+             - Syntax reduce(function, iterable)-
+'''
+
+#2. Explain the difference between higher order function, closure and decorator
+'''
+
+'''

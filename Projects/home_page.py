5075f6e85a5e1a017bf2335202deea70da366d5f
Mulat Yazew <gebremedhnyazewaait@gmail.com>
2025-07-20 12:49:43 +0300

    Day 17 of the 30 days Python challenge collab is completed

--- Projects/home_page.py
+++ Projects/home_page.py
@@ -1,2 +1,6 @@
-def home_page():
-    pass
+def student_home_page():
+    return "Welcome to the Student Home Page"
+def teacher_home_page():
+    return "Welcome to the Teacher Home Page"
+def admin_home_page():
+    return "Welcome to the Admin Home Page"

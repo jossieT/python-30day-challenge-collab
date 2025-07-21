be99224d24c4a8eac84f8389762a183e2848a149
jossieT <joseteshe2017@gmail.com>
2025-07-21 18:48:25 +0300

    day 15 pyhton type error exercise complete

--- exercises/day_16_python_date_time/python_date_time.py
+++ exercises/day_16_python_date_time/python_date_time.py
@@ -1,66 +1,42 @@
-from datetime import datetime
-#Exercises: Day 16
-#1. Get the current day, month, year, hour, minute and timestamp from datetime module
-now = datetime.now()
-current_date = now.day
-current_month = now.month
-current_year = now.year
-current_hour = now.hour
-current_minute = now.minute
-timestamp = now.timestamp()
+"""#SyntaxError
+#Example 1: SyntaxError
+print 'hello world' # Missing parentheses
 
-print(current_date, current_month, current_year)
-print(current_hour,":", current_minute)
-print(timestamp)
+#NameError
+#Example 1: NameError
+print(age) #age is not defined
 
-#2. Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
-formatted_date = now.strftime("%m/%d/%Y, %H:%M:%S")
-print(formatted_date)
+#IndexError
+#Example 1: IndexError
+numbers = [1, 2, 3, 4, 5]
+numbers[5] # list index out of range
 
-#3. Today is 5 December, 2019. Change this time string to time.
-string_time = "5 December, 2019"
-date_object = datetime.strptime(string_time, "%d %B, %Y")
-print(date_object)
+#ModuleNotFoundError
+#Example 1: ModuleNotFoundError
+import maths #No module named 'maths'
 
-#4. Calculate the time difference between now and new year.
-current_date = datetime.now()
-new_year = datetime(2026, 1, 1)
-diff = new_year - current_date
-print(diff)
+#AttributeError
+#Example 1: AttributeError
+import math
+math.PI # module 'math' has no attribute 'PI'
 
-#5. Calculate the time difference between 1 January 1970 and now.
-current_date = datetime.now()
-str_date = "1 January, 1970"
-date_object = datetime.strptime(str_date, "%d %B, %Y")
-difference = current_date - date_object
-print(difference)
+#KeyError
+#Example 1: KeyErrorKeyError
+users = {'name':'Asab', 'age':250, 'country':'Finland'}
+users['county'] #KeyError: 'county'
 
-#6. Think, what can you use the datetime module for? Examples:
-#Time series analysis
-#To get a timestamp of any activities in an application
-#Adding posts on a blog
-"""
-Calculating time differences
+#TypeError
+#Example 1: TypeError
+print(4 + '3') #TypeError: unsupported operand type(s) for +: 'int' and 'str'
 
-Scheduling tasks or events
+#ImportError
+#Example 1: TypeError
+from math import power #ImportError: cannot import name 'power' from 'math'
 
-Setting expiration dates
+#ValueError
+int('12a') # ValueError: invalid literal for int() with base 10: '12a'
 
-Naming files with timestamps
+#ZeroDivisionError
+1 / 0 #ZeroDivisionError: division by zero
 
-Calculating age or durations
-
-Converting between time zones
-
-Delaying execution or setting wait times
-
-Sorting records by date
-
-Logging events
-
-Generating reports with dates
-
-Filtering data by date ranges
-
-Measuring code execution time
 """

01d8fa73faecf51617f72d7d83d3e6c006319a87
Mulat Yazew <gebremedhnyazewaait@gmail.com>
2025-08-06 16:20:45 +0300

    20th day of 30 days Python challenge collab started

---
+++ exercises/day_20_python_package_manager/python_package_manager..py
@@ -0,0 +1,23 @@
+"""import webbrowser
+# list of urls: python
+url_lists = [
+    'http://www.python.org',
+    'https://www.linkedin.com/in/asabeneh/',
+    'https://github.com/Asabeneh',
+    'https://twitter.com/Asabeneh',
+]
+
+# opens the above list of websites in a different tab
+for url in url_lists:
+    webbrowser.open_new_tab(url)"""
+
+
+import requests # importing the request module
+
+url = 'https://www.py4e.com/code3/words.txt' # text from a website
+
+response = requests.get(url) # opening a network and fetching a data
+print(response)
+print(response.status_code) # status code, success:200
+print(response.headers)     # headers information
+print(response.text) # gives all the text from the page

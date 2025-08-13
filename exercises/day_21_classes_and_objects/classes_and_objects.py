a461d7eae51bb5b44935194a0153cccaf395a9fe
jossieT <joseteshe2017@gmail.com>
2025-08-13 23:05:46 +0300

    day 21 python class and object excersice update

--- exercises/day_21_classes_and_objects/classes_and_objects.py
+++ exercises/day_21_classes_and_objects/classes_and_objects.py
@@ -1,5 +1,5 @@
 from collections import Counter
-
+import math
 
 class Statistics:
     def __init__(self, data):
@@ -52,6 +52,34 @@ class Statistics:
             # Return the first mode if multiple exist (or single mode)
             return {'mode': modes[0], 'count': max_count}
 
+    def std(self):
+        n = len(self.data)
+        if n < 2:
+            return 0.0
+        mean = self.mean()
+        squared_diffs = [(x - mean) ** 2 for x in self.data]
+        variance = sum(squared_diffs) / n
+        return math.sqrt(variance)
+
+    def variance(self):
+        n = len(self.data)
+        mean = self.mean()
+        squared_diffs = [(x - mean) ** 2 for x in self.data]
+        variance = sum(squared_diffs) / n
+        return variance
+
+    def freq_dist(self):
+        count = len(self.data)
+        if count == 0:
+            return []
+
+        freq = Counter(self.data)
+        # Calculate percentages and create tuples (percentage, value)
+        distribution = [(round((v / count) * 100, 1), k) for k, v in freq.items()]
+        # Sort by percentage descending, then by value ascending
+        distribution.sort(key=lambda x: (-x[0], x[1]))
+        return distribution
+
 
 ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
 data = Statistics(ages)
@@ -64,7 +92,7 @@ print('Range: ', data.range())
 print('Mean: ', data.mean())
 print('Median: ', data.median())
 print('Mode: ', data.mode())
-
-
-
+print('Standard Deviation: ', data.std())
+print('Variance: ', data.variance())
+print('Frequency Distribution: ', data.freq_dist())
 
